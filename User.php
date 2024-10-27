cdc58f1d152200454d5812ea149cc3f3235188c2
Prince-sha <96621978+Prince-sha@users.noreply.github.com>
2024-10-27 18:15:57 +0500

    Updated User.php

--- User.php
+++ User.php
@@ -1,22 +1,28 @@
 <?php
 class User {
 
-    public static function create($username, $email, $password) {
+    public static function getDB() {
         $conn = new mysqli('localhost', 'root', '', 'codepro');
-
+        
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
+        
+        return $conn;
+    }
 
+    public static function create($username, $email, $password) {
+        $conn = self::getDB();
+        
         $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+        
         $hashed_password = password_hash($password, PASSWORD_DEFAULT);
         $stmt->bind_param("sss", $username, $email, $hashed_password);
 
-     
         if ($stmt->execute()) {
             echo "Registration successful!";
         } else {
-            echo "Error: " . $stmt->error;
+            echo "Error: ". $stmt->error;
         }
 
         $stmt->close();
