570d9ad31b7108fcde3b96adca70aa9eed8d07e8
Prince Sharma <96621978+Prince-sha@users.noreply.github.com>
2024-11-17 13:13:22 +0500

    Modified Database  dbconfig.php

--- Task2/dbconfig.php
+++ Task2/dbconfig.php
@@ -3,8 +3,9 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'codepro');
 define('DB_PASSWORD', 'princehash1');
-define('DB_NAME', 'announcement_db');
+define('DB_NAME', 'Database.php');
 // this is okay but we already had Database.php in classes folder.
+// Updated! 
 
 function getDBConnection() {
     $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
